c14bb7b9ffad8415945d79f7e4c2946757a4b7bc
Giorgi <gmaziashvili@live.com>
2020-12-12 13:46:23 +0400

    update samicda

--- samicda.cpp
+++ samicda.cpp
@@ -3,31 +3,47 @@ using namespace std;
 
 void dabechde(string sitkva)
 {
-  cout << sitkva << endl;
-  cout << "--------------------" << endl;
+  cout << " ----- " << sitkva << " ----- "<< endl;
+}
+
+void printSecret(){
+  dabechde("asdasdasdasdasdasdasd");
+  dabechde("xinkali");
+  dabechde("mwvadi");
+}
+
+bool authenticate(string REAL_PASSWORD)
+{
+  string inp;
+  dabechde("sheikvane paroli");
+  cin >> inp;
+  
+  int raodenoba = 0;
+  while (inp != REAL_PASSWORD)
+  {
+    cout<<"cda: "<<raodenoba+1<<endl;
+    raodenoba++;
+
+    if (raodenoba == 3)
+    {
+      dabechde("cda gamogelia");
+      return false;
+    }
+
+    dabechde("paroli arasworia, scade xelaxla");
+
+    cin >> inp;
+  }
+
+  return true;
 }
 
 int main()
 {
-  string inp;
-  dabechde("sheikvane paroli");
-  cin >> inp; 
-
-  int raodenoba = 0;
-  while (inp != "asd")
+  if (authenticate("kata"))
   {
-    raodenoba++;
-
-    if(raodenoba == 3){
-      dabechde("cda gamogelia");
-      return 0;
-    }
-
-    dabechde("paroli arasworia, scade xelaxla");
-    cin>>inp;
+    printSecret();
   }
-
-  dabechde("saidumlo informacia");
 
   return 0;
 }
