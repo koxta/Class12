2119d99460db96ed08bb798b13c5976d7cc6d2b9
Giorgi <gmaziashvili@live.com>
2020-12-19 13:33:09 +0400

    add sort print function

--- sortArr.cpp
+++ sortArr.cpp
@@ -4,6 +4,17 @@
 #include <algorithm>
 using namespace std;
 
+void printArr(int arr[], int size)
+{
+  int i = 0;
+  while (i < size)
+  {
+    cout << arr[i] << ", ";
+    i++;
+  }
+  cout << endl;
+}
+
 int main()
 {
   int n;
@@ -18,15 +29,13 @@ int main()
     i++;
   }
 
+  // dalagebamde
+  printArr(masivi, n);
+
   sort(masivi, masivi + n);
 
-  i = 0;
-  while (i < n)
-  {
-    cout << masivi[i] << ", ";
-    i++;
-  }
-  cout << endl;
+  // dalagebis shemdeg
+  printArr(masivi, n);
 
   return 0;
 }
